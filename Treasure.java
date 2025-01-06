f503720192837fa3e33966081a70996763f38730
Michelle <bths@ste.edu>
2025-01-06 10:52:28 -0500

    TH-1: Continued implementation of the Treasure class. Method getTreasure can now check whether or not the user own the item and the treasure now contains static booleans to check whether or not a treasure has been found and how many treasures in total are found.

--- Treasure.java
+++ Treasure.java
@@ -1,23 +1,51 @@
 public class Treasure {
 
-    private boolean ifFoundTreasure;
+    private boolean foundTreasure;
+    public static int totalTreasuresFound = 0;
+    private static boolean isVBAFound = false;
+    private static boolean isSCFound = false;
+    private static boolean isMFound = false;
 
     public String getTreasure() {
-        int x = (int) (Math.random() * 3) + 1;
-
-        if (x == 1 && !ifFoundTreasure) {
-            ifFoundTreasure = true;
+        if(foundTreasure) {
+            return "";
+        }
+        int x = (int) (Math.random() * 4) + 1;
+        foundTreasure = true;
+        if (x == 1) {
+            System.out.println("You found a Viking Battle Axe");
+            if (isVBAFound) {
+                System.out.println("You already own a Viking Battle Axe.");
+                System.out.println("You discarded the Battle Axe.");
+                return "";
+            }
+            totalTreasuresFound ++;
+            isVBAFound = true;
             return "Viking Battle Axe";
         }
-        else if (x == 2 && !) {
-            ifFoundTreasure = true;
+        else if (x == 2) {
+            if (isSCFound) {
+                System.out.println("You already own a Shieldmaiden Corset");
+                System.out.println("You discarded the Corset.");
+                return "";
+            }
+            totalTreasuresFound ++;
+            isSCFound = true;
             return "Shieldmaiden Corset";
         }
         else if (x == 3) {
-            ifFoundTreasure = true;
+            if (isMFound) {
+                System.out.println("You already own a Mjonir");
+                System.out.println("You discarded the Mjonir.");
+                return "";
+            }
+            totalTreasuresFound ++;
+            isMFound = true;
             return "Mjonir";
         }
-
+        else {
+            return "";
+        }
     }
 
 }
